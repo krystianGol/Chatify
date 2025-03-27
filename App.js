3194d1013d08882d864356ccfc7b56857cb1bbc8
krystianGol <krystianrafalgolda@gmail.com>
2025-03-27 16:27:45 +0100

    Load fonts funcionality

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, Button } from "react-native";
-import { useState, useEffect } from "react";
+import { useCallback, useEffect, useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
 export default function App() {
+
+  SplashScreen.preventAutoHideAsync();
+
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "black": require("./assets/fonts//Roboto-Black.ttf"),
+          "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          "bold": require("./assets/fonts/Roboto-Bold.ttf"),
+          "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          "italic": require("./assets/fonts/Roboto-Italic.ttf"),
+          "light": require("./assets/fonts/Roboto-Light.ttf"),
+          "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+          "medium": require("./assets/fonts/Roboto-Medium.ttf"),
+          "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          "regular": require("./assets/fonts/Roboto-Regular.ttf"),
+          "thin": require("./assets/fonts/Roboto-Thin.ttf"),
+          "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        })
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setTimeout(() => {
+          setAppIsLoaded(true);
+        }, 5000)
+      }
+    }
+    prepare();
+  }, [])
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaProvider style={styles.container}>
+    <SafeAreaProvider 
+      style={styles.container} 
+      onLayout={onLayout}>
       <SafeAreaView>
-        <Text>Hello world</Text>
+        <Text style={styles.label}>Hello world</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -20,4 +67,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  label: {
+    fontFamily: 'black',
+  }
 });
